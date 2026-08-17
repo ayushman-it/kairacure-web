28a517012c9114838f3f376bc1cec7cb7d800c39
Ayushman <avenuedesign768@gmail.com>
2026-08-17 13:49:34 +0530

    fix: import MedicalVideoBackdrop and define TRUST_METRICS in TrustStrip component

--- src/components/home/TrustStrip.jsx
+++ src/components/home/TrustStrip.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import { MedicalVideoBackdrop } from '../common/MedicalVideoBackdrop.jsx';
+
+const TRUST_METRICS = [
+  ['100k+', 'patient journeys benchmarked'],
+  ['38+', 'destination countries tracked'],
+  ['1,500+', 'hospital partners mapped'],
+  ['48h', 'medical opinion target'],
+];
 
 export function TrustStrip() {
   return (
