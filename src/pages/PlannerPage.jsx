1b4aa1093b2fbeb82bdc97bdaf2f80f47b54c054
Ayushman <avenuedesign768@gmail.com>
2026-08-17 14:01:45 +0530

    fix: import getTreatmentIconKind and treatment display helpers in PlannerPage

--- src/pages/PlannerPage.jsx
+++ src/pages/PlannerPage.jsx
@@ -21,7 +21,11 @@ import {
   normalizeSearch,
   getSearchOptionsFromData,
   withBackendTreatmentDefaults,
-  withBackendHospitalDefaults
+  withBackendHospitalDefaults,
+  getTreatmentIconKind,
+  getTreatmentDisplayTitle,
+  getTreatmentPageTitle,
+  buildTreatmentMeaning
 } from '../data/constants.js';
 
 export function PlannerPage({ hospitals = INDIA_HOSPITALS, initialProcedure = null, money = (value) => formatCurrency(value, 'INR'), selectedTreatment, selectedHospital, setPage, setSelectedHospital, setSelectedTreatment, treatments = TREATMENTS }) {
